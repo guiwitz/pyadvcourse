c96fb7722e36f938f39c47ff21fcd39a2a8493df
guiwitz <guiwitz@users.noreply.github.com>
2023-06-08 17:01:08 +0200

    versioning package

--- src/pyadvcourse/__init__.py
+++ src/pyadvcourse/__init__.py
@@ -1,5 +1,6 @@
 """Demo package for course"""
+from ._version import __version__
 
-__version__ = "0.0.1"
+#__version__ = "0.0.1"
 __author__ = "Guillaume Witz"
 __email__ = "guillaume.witz@unibe.ch"
